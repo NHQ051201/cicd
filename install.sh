17cdeae3f701b150a1a68d08c24a6ea3f1daa519
NHQ051201 <nguyenhoangquan.9420.15.16@gmail.com>
2025-01-08 10:30:36 +0000

    add setup for node-exporter, alertmanager, jfrog

--- install.sh
+++ install.sh
@@ -2,17 +2,23 @@
 
 echo "Starting to install"
 
+echo "--------------------------------------------------------"
 echo "Creating data"
-mkdir -p data/grafana data/jenkins data/loki data/prometheus data/promtail
+mkdir -p data/grafana data/jenkins data/loki data/prometheus data/promtail data/jfrog/artifactory data/alertmanager/config
+sudo chown -R 1030 ./data/jfrog/
 echo "Data has been created"
 
+echo "--------------------------------------------------------"
 echo "Copying essential configuration"
 cp loki-config.yml data/loki
 cp promtail-config.yml data/promtail
 cp prometheus.yml data/prometheus
+cp alertmanager.yml data/alertmanager/config
 echo "Done"
 
-echo "Run docker compose"
+echo "--------------------------------------------------------"
+echo "Start up containers"
 docker-compose up -d
 
+echo "--------------------------------------------------------"
 echo "All done and ready to use"
