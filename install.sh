197e2df9c24cb02a453d9f416d9649b238149f7e
NHQ051201 <nguyenhoangquan.9420.15.16@gmail.com>
2025-02-03 09:40:50 +0000

    added jfrog setup in install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,24 @@
 echo "Starting to install"
 
 echo "--------------------------------------------------------"
+echo "Downloading essential thing"
+DEFAULT_VERSION="7.49.8"
+read -p "Current version is $DEFAULT_VERSION. Do you want to change it? Default(Enter): N (y/N): " response
+if [[ "$response" =~ ^[Yy]$ ]]; then
+  read -p "Enter the custom version: " LATEST_VERSION
+else
+  LATEST_VERSION="$DEFAULT_VERSION"
+fi
+
+echo "Using JFrog Artifactory version: $LATEST_VERSION"
+curl -L "https://releases.jfrog.io/artifactory/bintray-artifactory/org/artifactory/jcr/docker/jfrog-artifactory-jcr/${LATEST_VERSION}/jfrog-artifactory-jcr-${LATEST_VERSION}-compose.tar.gz" -o jfrog-artifactory-compose.tar.gz
+latest_dir=$(tar -tf jfrog-artifactory-compose.tar.gz | head -n 1 | cut -d/ -f1)
+tar -xvzf jfrog-artifactory-compose.tar.gz
+rm -rf jfrog-artifactory-compose.tar.gz
+
+echo "--------------------------------------------------------"
 echo "Creating data"
-mkdir -p data/grafana data/jenkins data/loki data/prometheus data/promtail data/jfrog/artifactory data/alertmanager/config
-sudo chown -R 1030 ./data/jfrog/
+mkdir -p data/grafana data/jenkins data/loki data/prometheus data/promtail data/alertmanager/config
 echo "Data has been created"
 
 echo "--------------------------------------------------------"
@@ -18,6 +33,10 @@ echo "Done"
 
 echo "--------------------------------------------------------"
 echo "Start up containers"
+docker-compose up -d
+cd "$latest_dir"
+ls -l
+sudo ./config.sh
 docker-compose up -d
 
 echo "--------------------------------------------------------"
--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,6 @@
 echo "Starting to uninstall"
 
 docker-compose down -v
-sudo rm -rf data
+sudo rm -rf data artifactory*
 
 echo "Done"
