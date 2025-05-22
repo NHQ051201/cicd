0237a7a5ee3dc77e3c2ec9fb2a436c7b594e8d74
NHQ051201 <nguyenhoangquan.9420.15.16@gmail.com>
2025-05-22 08:22:37 +0000

    added install ansible script

---
+++ install_ansible.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Starting to install"
+
+echo "--------------------------------------------------------"
+echo "Creating data path"
+mkdir -p data/ansible
+echo "Data has been created"
+
+echo "--------------------------------------------------------"
+echo "Start up containers"
+cd ansible
+docker-compose up -d
+
+echo "--------------------------------------------------------"
+echo "Ansible is installed and ready"
