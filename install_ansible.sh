69e466fb24d46b7f05ac57f060b1091c46b2bb96
NHQ051201 <nguyenhoangquan.9420.15.16@gmail.com>
2025-05-25 15:22:33 +0000

    update install script

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
+set -e
+
 echo "Starting to install"
 
 echo "--------------------------------------------------------"
 echo "Creating data path"
 mkdir -p ~/ansible_config
 echo "Data has been created"
+
+echo "--------------------------------------------------------"
+echo "Setup command alias"
+FUNC_NAME="d-ansible"
+BASHRC="$HOME/.bashrc"
+if ! grep -q "$FUNC_NAME()" "$BASHRC"; then
+  cat << 'EOF' >> "$BASHRC"
+
+# Docker Ansible shortcut
+d-ansible() {
+  docker exec -it ansible bash -c "$*"
+}
+EOF
+  echo "Alias function '$FUNC_NAME' added to $BASHRC"
+else
+  echo "Alias function '$FUNC_NAME' already exists in $BASHRC"
+fi
 
 echo "--------------------------------------------------------"
 echo "Start up containers"
--- install_monitor.sh
+++ install_monitor.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
+set -e
+
 echo "Starting to install"
 
 echo "--------------------------------------------------------"
